fe06f36e02568bde7cc7240fee1073cac883dfc7
Revanthsatyam <satyamrevanth@gmail.com>
2024-11-21 12:40:10 +0530

    changes

---
+++ dispatch.sh
@@ -0,0 +1,15 @@
+dnf install golang -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+cd /app
+unzip /tmp/dispatch.zip
+cd /app
+go mod init dispatch
+go get
+go build
+cd /home/centos/roboshop-shell-d76
+cp dispatch.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl restart dispatch
