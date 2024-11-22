70df4783414f68cb63d867108cb33d16e43ec647
Revanthsatyam <satyamrevanth@gmail.com>
2024-11-22 19:38:11 +0530

    changes

--- common.sh
+++ common.sh
@@ -23,27 +23,25 @@ func_schema() {
     mongo --host mongodb-dev.rsdevops.in </app/schema/${component}.js &>>${log}
     func_exit_status
   fi
+
+  if [ "${schema_type}" == "mysql" ]; then
+    echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
+    dnf install mysql -y &>>${log}
+    mysql -h mysql-dev.rsdevops.in -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+    func_exit_status
+  fi
 }
 
-func_nodejs() {
-  echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
-  dnf module disable nodejs -y &>>${log}
-  dnf module enable nodejs:18 -y &>>${log}
-  dnf install nodejs -y &>>${log}
-  func_exit_status
-
+func_appreq() {
   echo -e "\e[36m>>>>>>>>> Copy ${component} Service <<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service
   func_exit_status
 
-  if [ "${schema_enabled}" == "yes" ]; then
-    echo -e "\e[36m>>>>>>>>> Copy Mongo Repo <<<<<<<<<<\e[0m"
-    cp mongo.repo /etc/yum.repos.d/mongo.repo
-    func_exit_status
-  fi
-
   echo -e "\e[36m>>>>>>>>> Create User <<<<<<<<<<\e[0m"
-  useradd roboshop &>>${log}
+  id roboshop &>>${log}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${log}
+  fi
   func_exit_status
 
   echo -e "\e[36m>>>>>>>>> Download Application Content <<<<<<<<<<\e[0m"
@@ -55,9 +53,44 @@ func_nodejs() {
   cd /app
   unzip /tmp/${component}.zip &>>${log}
   func_exit_status
+}
+
+func_nodejs() {
+  echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
+  dnf module disable nodejs -y &>>${log}
+  dnf module enable nodejs:18 -y &>>${log}
+  dnf install nodejs -y &>>${log}
+  func_exit_status
+
+  if [ "${schema_enabled}" == "yes" ]; then
+    echo -e "\e[36m>>>>>>>>> Copy Mongo Repo <<<<<<<<<<\e[0m"
+    cp mongo.repo /etc/yum.repos.d/mongo.repo
+    func_exit_status
+  fi
+
+  func_appreq
 
   echo -e "\e[36m>>>>>>>>> Download Dependencies <<<<<<<<<<\e[0m"
   npm install &>>${log}
+  func_exit_status
+
+  func_systemctl
+
+  if [ "${schema_enabled}" == "yes" ]; then
+    func_schema
+  fi
+}
+
+func_java() {
+  echo -e "\e[36m>>>>>>>>> Install Maven <<<<<<<<<<\e[0m"
+  dnf install maven -y &>>${log}
+  func_exit_status
+
+  func_appreq
+
+  echo -e "\e[36m>>>>>>>>> Download Dependencies <<<<<<<<<<\e[0m"
+  mvn clean package &>>${log}
+  mv target/shipping-1.0.jar shipping.jar &>>${log}
   func_exit_status
 
   func_systemctl
--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,25 @@
-dnf install maven -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-cd /home/centos/roboshop-shell-d76
-cp shipping.service /etc/systemd/system/shipping.service
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
-dnf install mysql -y
-mysql -h mysql-dev.rsdevops.in -uroot -pRoboShop@1 < /app/schema/shipping.sql
-systemctl restart shipping
+source common.sh
+
+component=shipping
+schema_enabled=yes
+schema_type=mysql
+
+func_java
+
+#dnf install maven -y
+#useradd roboshop
+#mkdir /app
+#curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+#cd /app
+#unzip /tmp/shipping.zip
+#cd /app
+#mvn clean package
+#mv target/shipping-1.0.jar shipping.jar
+#cd /home/centos/roboshop-shell-d76
+#cp shipping.service /etc/systemd/system/shipping.service
+#systemctl daemon-reload
+#systemctl enable shipping
+#systemctl restart shipping
+#dnf install mysql -y
+#mysql -h mysql-dev.rsdevops.in -uroot -pRoboShop@1 < /app/schema/shipping.sql
+#systemctl restart shipping
