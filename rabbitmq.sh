84fb1d0b59a42c51378b46eb40edec84a5f09d05
Revanthsatyam <satyamrevanth@gmail.com>
2024-11-22 19:48:45 +0530

    changes

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,12 @@
+rabbitmq_pass=$1
+if [ -z ${rabbitmq_pass} ]; then
+  echo "Password Missing! Input Password"
+  exit 1
+fi
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 dnf install rabbitmq-server -y
 systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop ${rabbitmq_pass}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
