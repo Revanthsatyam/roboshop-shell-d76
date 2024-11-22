d489b0115b2822642d2f7e3976c9739ca0f5d4d8
Revanthsatyam <satyamrevanth@gmail.com>
2024-11-22 20:56:45 +0530

    changes

--- common.sh
+++ common.sh
@@ -18,15 +18,21 @@ func_systemctl() {
 
 func_schema() {
   if [ "${schema_type}" == "mongodb" ]; then
-    echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
+    echo -e "\e[36m>>>>>>>>> Install Mongo Client <<<<<<<<<<\e[0m"
     dnf install mongodb-org-shell -y &>>${log}
+    func_exit_status
+
+    echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
     mongo --host mongodb-dev.rsdevops.in </app/schema/${component}.js &>>${log}
     func_exit_status
   fi
 
   if [ "${schema_type}" == "mysql" ]; then
-    echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
+    echo -e "\e[36m>>>>>>>>> Install MySQL Client <<<<<<<<<<\e[0m"
     dnf install mysql -y &>>${log}
+    func_exit_status
+
+    echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
     mysql -h mysql-dev.rsdevops.in -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
     func_exit_status
   fi
