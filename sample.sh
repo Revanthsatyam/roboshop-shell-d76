6212f141c8c6ab96ab78f88afb14da014e4fe8d3
Revanthsatyam <satyamrevanth@gmail.com>
2024-11-23 14:39:05 +0530

    changes

--- sample.sh
+++ sample.sh
@@ -5,10 +5,11 @@ b=0
 c=0
 
 for i in 0..${day}; do
-  a=$((a+1))
-  echo $a
-  b=$((a+1))
-  echo $b
-  c=$((b+1))
-  echo $c
+#  a=$((a+1))
+#  echo $a
+#  b=$((a+1))
+#  echo $b
+#  c=$((b+1))
+#  echo $c
+   echo $i
 done
