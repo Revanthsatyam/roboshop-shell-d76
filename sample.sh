7607e6513ddd75d8091b7971a31d7c1ccc65c7e5
Revanthsatyam <satyamrevanth@gmail.com>
2024-11-23 14:58:30 +0530

    changes

--- sample.sh
+++ sample.sh
@@ -5,9 +5,9 @@ b=0
 c=0
 
 for (( i = 1; i <= day; i++ )); do
-  a=$((a+1))
-  b=$((a+1))
-  c=$((b+1))
+  a=$((a+1)) #1 1 2 2
+  b=$((a+1)) #2 1 3 2
+  c=$((b+1)) #3 1 4 2
   if [ $i -eq ${day} ]; then
     echo "Max Height for the day is =$c"
     echo "BaseCamp for the day is =$a"
