ee8afed21858e6872e0f0b373ba780b0f79094bf
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-04 16:45:00 +0530

    changes

--- catalogue.sh
+++ catalogue.sh
@@ -8,3 +8,10 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app
 npm install
+cp catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-org-shell -y
+mongo --host mongodb-dev.rsdevops.in </app/schema/catalogue.js
