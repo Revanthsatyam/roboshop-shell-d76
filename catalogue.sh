3be14b9a9a3fb7f72c9186181d877048fbdc43e2
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-04 16:46:45 +0530

    changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,3 +1,4 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
@@ -8,7 +9,6 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
