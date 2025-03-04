0d7dff172f3ac5c116005eb04b77da800859bc61
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-04 16:57:42 +0530

    changes

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
@@ -5,5 +6,4 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
