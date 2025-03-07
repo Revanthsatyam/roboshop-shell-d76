c668dd373ddff8868a6ef6a641f0a2e18bf86a6c
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-07 19:31:12 +0530

    changes

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,14 @@
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo "Missing MySQL Password"
+  exit 1
+fi
+
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 dnf module disable mysql -y
 dnf install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_root_password}
+
+#RoboShop@1
