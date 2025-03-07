8eb6b793754f04c21029478987b09f3d9026123d
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-07 16:43:16 +0530

    changes

--- common.sh
+++ common.sh
@@ -2,33 +2,33 @@ log=/tmp/roboshop.log
 
 func_nodejs() {
   echo -e "\e[36m>>>>>>>>> Copy ${component} Service <<<<<<<<<<\e[0m"
-  cp catalogue.service /etc/systemd/system/catalogue.service
-  cp mongo.repo /etc/yum.repos.d/mongo.repo
+  cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
   echo -e "\e[36m>>>>>>>>> Install NodeJS Service <<<<<<<<<<\e[0m"
-  dnf module disable nodejs -y
-  dnf module enable nodejs:18 -y
-  dnf install nodejs -y
+  dnf module disable nodejs -y &>>${log}
+  dnf module enable nodejs:18 -y &>>${log}
+  dnf install nodejs -y &>>${log}
 
   echo -e "\e[36m>>>>>>>>> Create Roboshop User <<<<<<<<<<\e[0m"
-  useradd roboshop
-  mkdir /app
+  useradd roboshop &>>${log}
+  mkdir /app &>>${log}
 
   echo -e "\e[36m>>>>>>>>> Download ${component} Artifacts <<<<<<<<<<\e[0m"
-  curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+  curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
   cd /app
-  unzip /tmp/catalogue.zip
+  unzip /tmp/catalogue.zip &>>${log}
   cd /app
 
   echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<<<\e[0m"
-  npm install
+  npm install &>>${log}
 
   echo -e "\e[36m>>>>>>>>> Start ${component} Service <<<<<<<<<<\e[0m"
-  systemctl daemon-reload
-  systemctl enable catalogue
-  systemctl start catalogue
+  systemctl daemon-reload &>>${log}
+  systemctl enable catalogue &>>${log}
+  systemctl start catalogue &>>${log}
 
   echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
-  dnf install mongodb-org-shell -y
-  mongo --host mongodb-dev.rsdevops.in </app/schema/catalogue.js
+  dnf install mongodb-org-shell -y &>>${log}
+  mongo --host mongodb-dev.rsdevops.in </app/schema/catalogue.js &>>${log}
 }
