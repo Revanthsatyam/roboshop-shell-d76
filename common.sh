59085bdfd36769bd7339df3ec97fd31ac6816280
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-07 16:32:05 +0530

    changes

--- common.sh
+++ common.sh
@@ -12,9 +12,9 @@ func_nodejs() {
 
   echo -e "\e[36m>>>>>>>>> Create Roboshop User <<<<<<<<<<\e[0m"
   useradd roboshop
+  mkdir /app
 
   echo -e "\e[36m>>>>>>>>> Download ${component} Artifacts <<<<<<<<<<\e[0m"
-  mkdir /app
   curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
   cd /app
   unzip /tmp/catalogue.zip
@@ -22,4 +22,13 @@ func_nodejs() {
 
   echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<<<\e[0m"
   npm install
+
+  echo -e "\e[36m>>>>>>>>> Start ${component} Service <<<<<<<<<<\e[0m"
+  systemctl daemon-reload
+  systemctl enable catalogue
+  systemctl start catalogue
+
+  echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
+  dnf install mongodb-org-shell -y
+  mongo --host mongodb-dev.rsdevops.in </app/schema/catalogue.js
 }
