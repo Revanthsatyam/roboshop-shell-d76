ad6adbc13cb1509ec23aef5dcd9f5417d3390d8b
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-07 18:39:54 +0530

    changes

--- common.sh
+++ common.sh
@@ -8,6 +8,14 @@ func_exit_stat() {
   fi
 }
 
+func_sysd() {
+  echo -e "\e[36m>>>>>>>>> Start ${component} Service <<<<<<<<<<\e[0m"
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl start ${component} &>>${log}
+  func_exit_stat
+}
+
 func_nodejs() {
   echo -e "\e[36m>>>>>>>>> Copy ${component} Service <<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
@@ -39,7 +47,7 @@ func_nodejs() {
   func_exit_status
 
   echo -e "\e[36m>>>>>>>>> Download ${component} Artifacts <<<<<<<<<<\e[0m"
-  curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
   cd /app
   unzip /tmp/${component}.zip &>>${log}
   func_exit_stat
@@ -48,11 +56,7 @@ func_nodejs() {
   npm install &>>${log}
   func_exit_stat
 
-  echo -e "\e[36m>>>>>>>>> Start ${component} Service <<<<<<<<<<\e[0m"
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  systemctl start ${component} &>>${log}
-  func_exit_stat
+  func_sysd
 
   if [ "${component}" == catalogue ]; then
     echo -e "\e[36m>>>>>>>>> Install Mongo Client <<<<<<<<<<\e[0m"
@@ -63,4 +67,51 @@ func_nodejs() {
     mongo --host mongodb-dev.rsdevops.in </app/schema/catalogue.js &>>${log}
     func_exit_stat
   fi
+}
+
+func_java() {
+  echo -e "\e[36m>>>>>>>>> Copy ${component} Service <<<<<<<<<<\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+  func_exit_stat
+
+  echo -e "\e[36m>>>>>>>>> Install Java <<<<<<<<<<\e[0m"
+  dnf install maven -y &>>${log}
+  func_exit_stat
+
+  echo -e "\e[36m>>>>>>>>> Create Roboshop User <<<<<<<<<<\e[0m"
+  id roboshop &>>${log}
+  if [ "$?" != 0 ]; then
+    useradd roboshop &>>${log}
+  fi
+  func_exit_stat
+
+  echo -e "\e[36m>>>>>>>>> Cleanup Existing Application Content <<<<<<<<<<\e[0m"
+  rm -rf /app &>>${log}
+  func_exit_stat
+
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application Directory  <<<<<<<<<<<<\e[0m"
+  mkdir /app &>>${log}
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>> Download ${component} Artifacts <<<<<<<<<<\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  cd /app
+  unzip /tmp/${component}.zip &>>${log}
+  func_exit_stat
+
+  echo -e "\e[36m>>>>>>>>> Install Dependencies <<<<<<<<<<\e[0m"
+  mvn clean package &>>${log}
+  mv target/shipping-1.0.jar shipping.jar &>>${log}
+  func_exit_stat
+
+  func_sysd
+
+  echo -e "\e[36m>>>>>>>>> Install MySQL Client <<<<<<<<<<\e[0m"
+  dnf install mysql -y &>>${log}
+  func_exit_stat
+
+  echo -e "\e[36m>>>>>>>>> Load Schema <<<<<<<<<<\e[0m"
+  mysql -h mysql-dev.rsdevops.in -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>${log}
+  systemctl restart shipping &>>${log}
+  func_exit_stat
 }
